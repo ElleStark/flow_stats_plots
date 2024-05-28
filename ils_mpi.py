4c6fc4d46f1f36c6ffe05b68dc885981974a2201
ElleStark <elle.stark5645@gmail.com>
2024-05-28 17:11:04 -0600

    compute n elements per process

--- ils_mpi.py
+++ ils_mpi.py
@@ -26,35 +26,50 @@ out_path = '/rc_scratch/elst4602/tests/uStatTests.npy'
 comm_world = MPI.COMM_WORLD
 my_rank = comm_world.Get_rank()
 # number of processes will be determined from Slurm job script (.sh file)
-# ntasks for this script ideally inteded to be ~1001 (using pre-emptable Blanca nodes) 
+# ntasks for this script ideally inteded to be ~1000 (using pre-emptable Blanca nodes) 
 num_procs = comm_world.Get_size()
+print('RUNNING ON {num_procs} CORES.'.format(num_procs=num_procs))
+
 
 if (my_rank==0):
     # process 0 reads in data from PetaLibrary
-    DEBUG("Reading data from PetaLibrary")
+    DEBUG('Reading data from PetaLibrary')
 
     start_t = time.time()
 
     file_path = '/pl/active/odor2action/Data/odorPlumeData/multisource-plume-simulations/hdf5/Re100_0_5mm_50Hz_16source_FTLE_manuscript.h5'
 
     with h5py.File(file_path, 'r') as f:
-        u_data = f.get('Flow Data/u')[:]  # [time, x, y] array with dims [3001, 1001, 846]
+        u_data = f.get('Flow Data/u')[:-1, :, :].transpose(1, 2, 0)  # original: [time, x, y] array with dims [3001, 1001, 846]
 
     total_t = time.time() - start_t
-    INFO("u array read from PetaLibrary in {total_t} sec".format(total_t=total_t))
+    INFO('u array read from PetaLibrary in {total_t} sec'.format(total_t=total_t))
 
     u_slice = u_data[0, 0, 1:10]
-    DEBUG("First 10 vals of u array: {u_slice}".format(u_slice=u_slice))
+    DEBUG('First 10 vals of u array: {u_slice}'.format(u_slice=u_slice))
 
     # np.save(out_path, u_slice)
 
-# broadcast u data from process 0 into variable 'u_data' variable in other processes
-u_data = comm_world.bcast(u_data, root=0)
+# Compute number of elements per spatial grid and per processor
+n_grid = u_data[:, :, 0].size
+tsteps_per_proc = len(u_data[0, 0, :]) // num_procs
+n_per_proc = n_grid * tsteps_per_proc
+
+
+# Buffer array to fill in each process
+u_chunk = np.empty(n_per_proc, dtype=np.float64)
+
+# scatter u data from process 0 into variable 'u_data' variable in other processes
+comm_world.Scatter([u_data, MPI.DOUBLE], [u_chunk, MPI.DOUBLE])
 
 # INTEGRAL LENGTH SCALE COMPUTATION FOR EACH PROCESS:
-# subset domain for computing ILS, selecting column based on rank of process
-
-# each process returns 846 computed ILS values for the selected column
+# subset domain for computing ILS, selecting time step based on rank of process
 
 
+
+# each process returns computed ILS matrix for the selected time step
+#if (my_rank != 0):
+ #   comm_world.gather(ils_col, dest=0, tag=1)
+#else:
+    # average all timesteps to get ILS at each point
 
