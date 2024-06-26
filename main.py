8eb195a1464cb390ea16b0e493de7c08ff8e68f6
ElleStark <elle.stark5645@gmail.com>
2024-06-26 07:20:44 -0600

    compute reynolds stresses

--- main.py
+++ main.py
@@ -21,17 +21,15 @@ def main():
         y_grid = f.get(f'Model Metadata/yGrid')[:].T
 
         # u and v velocity field data
-        # u_data = f.get('Flow Data/u')[:].transpose(0, 2, 1)
-        # v_data = f.get('Flow Data/v')[:].transpose(0, 2, 1)
+        u_data = f.get('Flow Data/u')[:].transpose(0, 2, 1)
+        v_data = f.get('Flow Data/v')[:].transpose(0, 2, 1)
 
-        # # Spatial resolution
-        # dx = f.get('Model Metadata/spatialResolution')[0].item()
+        # Spatial resolution
+        dx = f.get('Model Metadata/spatialResolution')[0].item()
 
-    # # Decompose velocity fields into mean and fluctuating components of u and v
-    # u_mean, u_flx = utils.reynolds_decomp(u_data, time_ax=0)
-    # v_mean, v_flx = utils.reynolds_decomp(v_data, time_ax=0)
-
-    # # Compute turbulence intensity
+    # Decompose velocity fields into mean and fluctuating components of u and v
+    u_mean, u_flx = utils.reynolds_decomp(u_data, time_ax=0)
+    v_mean, v_flx = utils.reynolds_decomp(v_data, time_ax=0)
 
 
     # # Compute turbulent kinetic energy
@@ -49,25 +47,38 @@ def main():
     # utils.plot_field_xy(x_grid, np.flipud(y_grid), v_mean, cmap=cmap, title='mean v', filepath='ignore/v_mean.png', dpi=600)
 
     # # PLOTS: integral scales
-    ils_uxstream = np.load('ignore/ILS_u_cross_stream.npy')
-    ils_vstream = np.load('ignore/ILS_vstreamwise.npy')
-    ils_ustream = np.load('ignore/ILS_ustreamwise.npy')
-    ils_vxstream = np.load('ignore/ILS_v_cross_stream.npy')
+    # ils_uxstream = np.load('ignore/ILS_u_cross_stream.npy')
+    # ils_vstream = np.load('ignore/ILS_vstreamwise.npy')
+    # ils_ustream = np.load('ignore/ILS_ustreamwise.npy')
+    # ils_vxstream = np.load('ignore/ILS_v_cross_stream.npy')
 
-    cmap = cmr.gothic_r
-    utils.plot_field_xy(x_grid, y_grid, ils_uxstream,  title='Integral Length Scale, u cross-stream', cmap=cmap, range=[0, 0.15], filepath='ignore/ils_u_cross_stream_trimmed_alt.png', dpi=600, trimmed=True)
-    utils.plot_field_xy(x_grid, y_grid, ils_vstream,  title='Integral Length Scale, v streamwise', cmap=cmap, range=[0, 0.15], filepath='ignore/ils_v_streamwise_trimmed_alt.png', dpi=600, trimmed=True)
+    # cmap = cmr.gothic_r
+    # utils.plot_field_xy(x_grid, y_grid, ils_uxstream,  title='Integral Length Scale, u cross-stream', cmap=cmap, range=[0, 0.15], filepath='ignore/ils_u_cross_stream_trimmed_alt.png', dpi=600, trimmed=True)
+    # utils.plot_field_xy(x_grid, y_grid, ils_vstream,  title='Integral Length Scale, v streamwise', cmap=cmap, range=[0, 0.15], filepath='ignore/ils_v_streamwise_trimmed_alt.png', dpi=600, trimmed=True)
     # utils.plot_field_xy(x_grid, y_grid, ils_ustream,  title='Integral Length Scale, u streamwise', cmap=cmap, range=[0, 0.15], filepath='ignore/ils_u_streamwise_trimmed.png', dpi=600, trimmed=True)
     # utils.plot_field_xy(x_grid, y_grid, ils_vxstream,  title='Integral Length Scale, v cross-stream', cmap=cmap, range=[0, 0.15], filepath='ignore/ils_v_cross_stream_trimmed.png', dpi=600, trimmed=True)
  
-    its_u = scipy.io.loadmat('ignore/Tux_array_u_update.mat')
-    its_u = np.array(its_u['Tux_array'])
-    its_v = scipy.io.loadmat('ignore/Tux_array_v.mat')
-    its_v = np.array(its_v['Tux_array'])
+    # its_u = scipy.io.loadmat('ignore/Tux_array_u_update.mat')
+    # its_u = np.array(its_u['Tux_array'])
+    # its_v = scipy.io.loadmat('ignore/Tux_array_v.mat')
+    # its_v = np.array(its_v['Tux_array'])
 
-    cmap = cmr.jungle_r
+    # cmap = cmr.jungle_r
     # utils.plot_field_xy(x_grid, y_grid, its_u, title='Integral Time Scale, u', cmap=cmap, range=[0, 1.5], filepath='ignore/its_u_trimmed.png', dpi=600, smooth=True, trimmed=True)
-    utils.plot_field_xy(x_grid, y_grid, its_v, title='Integral Time Scale, v', cmap=cmap, range=[0, 1.5], filepath='ignore/its_v_trimmed_alt.png', dpi=600, smooth=True, trimmed=True)
+    # utils.plot_field_xy(x_grid, y_grid, its_v, title='Integral Time Scale, v', cmap=cmap, range=[0, 1.5], filepath='ignore/its_v_trimmed_alt.png', dpi=600, smooth=True, trimmed=True)
+
+
+    # Compute Reynolds Stress Tensor
+    u_prime_sq = np.mean(u_flx**2, axis=0)
+    v_prime_sq = np.mean(v_flx**2, axis=0)
+    uv_prime = np.mean(u_flx*v_flx, axis=0)
+
+    # PLOTS: components of Reynolds stress
+    cmap = cmr.tree
+    utils.plot_field_xy(x_grid, y_grid, u_prime_sq, title='Reynolds Stress: u normal', cmap=cmap, filepath='ignore/rs_uprimesq.png', dpi=600)
+    utils.plot_field_xy(x_grid, y_grid, v_prime_sq, title='Reynolds Stress: v normal', cmap=cmap, filepath='ignore/rs_vprimesq.png', dpi=600)
+    utils.plot_field_xy(x_grid, y_grid, uv_prime, title='Reynolds Stress: uv shear', cmap=cmap, filepath='ignore/rs_uvprime.png', dpi=600)
+ 
 
 
 if __name__ == '__main__':
